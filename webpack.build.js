7c3a13b7fe222f05346d89b0d651bb20dbec5313
Arkhip Zharov <arkhipzharov627@gmail.com>
2019-06-14 16:29:11 +0300

    fix: cайт не обновлялся на телефонах
    После обновления сайта на хостинге на мобильных загружалась его старая
    версия

--- webpack.build.js
+++ webpack.build.js
@@ -104,7 +104,7 @@ module.exports = merge(baseConfig, {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: 'style.[hash].css',
     }),
       // JSON файлы статичные
     new CopyPlugin([
--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'main.[hash].js',
   },
   resolve: {
     alias: {
